8d71056e5a8b1f523f9049fa37badc2b5a132764
nick800103 <keepprogress@gmail.com>
2020-12-15 17:51:09 +0800

    refactor - place advice to seperated Aspect

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -9,30 +9,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
 		
-	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
-	private void forDaoPackage() {}
-		
-	// create pointcut for getter methods
-	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
-	public void getter() {}
-	
-	// create pointcut for setter methods
-	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
-	public void setter() {}
-
-	// create point: include package ... exclude getter/setter
-	@Pointcut("forDaoPackage() && !(getter() || setter())")
-	private void forDaoPackageNoGetterSetter() {}
-	
-	
 	@Before("forDaoPackageNoGetterSetter()")
 	public void AddAccountAdvice() {		
 		System.out.println("\n=====>>> Excuting @Before advice on addAccount()");
-	}
-	
-	@Before("forDaoPackageNoGetterSetter()")
-	public void performApiAnalytic() {
-		System.out.println("\n=====>>> Peforming API analytics");
 	}
 
 }
